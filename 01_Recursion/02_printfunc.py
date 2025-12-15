7717832421f93a1c7fe3a4f36089d1c364b6a392
Isha01-S <ishamca01@gmail.com>
2025-12-15 11:13:10 +0530

    added python code for factorial

---
+++ 01_Recursion/02_printfunc.py
@@ -0,0 +1,17 @@
+
+
+def Factorial(n):
+
+    if (n==1 or n==0):
+        return 1
+    
+    else:
+        return n*Factorial(n-1)
+    
+
+
+n= int(input("Enter n: "))
+factorial=Factorial(n)
+print(f"Factoria of {n} is: {factorial}")
+
+
